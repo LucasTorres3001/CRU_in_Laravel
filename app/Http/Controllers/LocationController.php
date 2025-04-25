d9b1b0072b78dac4a5df3ff1188807ab4de8c202
Lucas Torres <lukas.torres30.lt@gmail.com>
2025-04-25 20:41:25 -0300

    second commit

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -20,35 +20,39 @@ class LocationController extends Controller
      */
     public function create()
     {
-        $locations = [
-            'cities' => [
-                'São Paulo', 'Rio de Janeiro', 'Brasília', 'Salvador', 'Fortaleza', 'Belo Horizonte', 'Manaus', 'Curitiba', 'Recife',
-                'Goiânia', 'Belém', 'Porto Alegre', 'São Luís', 'Maceió', 'Campo Grande', 'Natal', 'Teresina', 'João Pessoa', 'Aracaju',
-                'Cuiabá', 'Porto Velho', 'Macapá', 'Florianópolis', 'Boa Vista', 'Rio Branco', 'Vitória', 'Palmas', 'Guarulhos', 'Niterói',
-                'Uberlândia', 'Campinas', 'Londrina', 'Caxias do Sul', 'São Bernardo do Campo', 'Duque de Caxias', 'Contagem', 'Maringá',
-                'Pelotas', 'Joinville', 'Vila Velha'
-            ],
-            'states' => [
-                'AC' => 'Acre', 'AL' => 'Alagoas', 'AP' => 'Amapá', 'AM' => 'Amazonas', 'BA' => 'Bahia', 'CE' => 'Ceará',
-                'DF' => 'Distrito Federal', 'ES' => 'Espirito Santo', 'GO' => 'Goiás', 'MA' => 'Maranhão', 'MT' => 'Mato Grosso',
-                'MS' => 'Mato Grosso do Sul', 'MG' => 'Minas Gerais', 'PA' => 'Pará', 'PB' => 'Paraíba', 'PR' => 'Paraná',
-                'PE' => 'Pernambuco', 'PI' => 'Piauí', 'RJ' => 'Rio de Janeiro', 'RN' => 'Rio Grande do Norte', 'RS' => 'Rio Grande do Sul',
-                'RO' => 'Rondônia', 'RR' => 'Roraíma', 'SC' => 'Santa Catarina', 'SP' => 'São Paulo', 'SE' => 'Sergipe', 'TO' => 'Tocantins'
-            ],
-            'regions' => [
-                'Midwest', 'North', 'North East', 'South', 'Southeast'
+        return view('locations.create',
+            [
+                'locations' => [
+                    'cities' => [
+                        'São Paulo', 'Rio de Janeiro', 'Brasília', 'Salvador', 'Fortaleza', 'Belo Horizonte', 'Manaus', 'Curitiba', 'Recife',
+                        'Goiânia', 'Belém', 'Porto Alegre', 'São Luís', 'Maceió', 'Campo Grande', 'Natal', 'Teresina', 'João Pessoa', 'Aracaju',
+                        'Cuiabá', 'Porto Velho', 'Macapá', 'Florianópolis', 'Boa Vista', 'Rio Branco', 'Vitória', 'Palmas', 'Guarulhos', 'Niterói',
+                        'Uberlândia', 'Campinas', 'Londrina', 'Caxias do Sul', 'São Bernardo do Campo', 'Duque de Caxias', 'Contagem', 'Maringá',
+                        'Pelotas', 'Joinville', 'Vila Velha'
+                    ],
+                    'states' => [
+                        'AC' => 'Acre', 'AL' => 'Alagoas', 'AP' => 'Amapá', 'AM' => 'Amazonas', 'BA' => 'Bahia', 'CE' => 'Ceará',
+                        'DF' => 'Distrito Federal', 'ES' => 'Espirito Santo', 'GO' => 'Goiás', 'MA' => 'Maranhão', 'MT' => 'Mato Grosso',
+                        'MS' => 'Mato Grosso do Sul', 'MG' => 'Minas Gerais', 'PA' => 'Pará', 'PB' => 'Paraíba', 'PR' => 'Paraná',
+                        'PE' => 'Pernambuco', 'PI' => 'Piauí', 'RJ' => 'Rio de Janeiro', 'RN' => 'Rio Grande do Norte',
+                        'RS' => 'Rio Grande do Sul', 'RO' => 'Rondônia', 'RR' => 'Roraíma', 'SC' => 'Santa Catarina', 'SP' => 'São Paulo',
+                        'SE' => 'Sergipe', 'TO' => 'Tocantins'
+                    ],
+                    'regions' => [
+                        'Midwest', 'North', 'North East', 'South', 'Southeast'
+                    ]
+                ]
             ]
-        ];
-        return view('locations.create', ['locations' => $locations]);
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreLocationRequest $requestLocation)
+    public function store(StoreLocationRequest $storeLocationRequest)
     {
         Location::create(
-            $requestLocation->validated()
+            $storeLocationRequest->validated()
         );
         return redirect()->route('locations.store')->with('success', 'Location registered successfully!');
     }
